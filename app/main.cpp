ab9a9f60f0c9da0f6a8e3c2c9b97b887485ca996
Lewis Sneddon <lewis.sneddon@icloud.com>
2023-09-16 23:44:15 +0100

    Removed redundant include headers

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,6 @@
 // Executables must have the following defined if the library contains
 // doctest definitions. For builds with this disabled, e.g. code shipped to
 // users, this can be left out.
-#include "engine.h"
-#include "game.h"
-#include "wordFactory.h"
 #ifdef ENABLE_DOCTEST_IN_LIBRARY
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
